cca8376d95dae62aa8cb21c4b1abb087e69dd873
Melissa Mayfield <Mayfieldmel@gmail.com>
2023-05-15 19:55:15 +0000

    test passed

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -14,7 +14,6 @@ public class JavalinSingleton {
     public static Javalin getInstance(){
         Javalin app = Javalin.create();
         
-
         app.get("/client-side-err", ctx -> {
         
             ctx.status(400);
